ad75fecb6db37444e19d089a7b9afb3516f58d8e
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-07-09 12:16:54 -0400

    bioclimatic distance map done

--- src/MangalSuppMat.jl
+++ src/MangalSuppMat.jl
@@ -1,5 +1,51 @@
 module MangalSuppMat
 
+using Statistics
+using SimpleSDMLayers
+
 greet() = print("Hello World!")
+
+function haversine(p1, p2, r)
+	return acos(sin(p1[2])*sin(p2[2])+cos(p1[2])*cos(p2[2])*cos(p2[1]-p1[1]))*r
+end
+export haversine
+
+"""
+Normalizes a layer (z-score)
+"""
+function z!(p::T) where {T <: SimpleSDMResponse}
+   d = filter(!isnan, p.grid)
+   μ = mean(d)
+   σ = std(d)
+	for i in eachindex(p.grid)
+		if !isnan(p[i])
+			p.grid[i] = (p.grid[i]-μ)/σ
+		end
+	end
+end
+
+"""
+Normalizes a copy of the layer
+"""
+function z(p::T) where {T <: SimpleSDMResponse}
+   y = copy(p)
+   z!(y)
+   return y
+end
+
+function z(p::T) where {T <: SimpleSDMPredictor}
+   return convert(SimpleSDMPredictor, z(convert(SimpleSDMResponse, p)))
+end
+
+"""
+Normalizes a value (z-score)
+"""
+function z(v, p::T) where {T <: SimpleSDMLayer}
+   @assert typeof(v) <: Number
+   d = filter(!isnan, p.grid)
+   return (v-mean(d))/std(d)
+end
+
+export z, z!
 
 end # module
--- test/04_analog_maps.jl
+++ test/04_analog_maps.jl
@@ -1,9 +1,10 @@
 module MangalSuppMatAnalogMaps
 
+using MangalSuppMat
+
 using DataFrames
 using CSV
 using Plots
-# using Distances
 using SimpleSDMLayers
 using Statistics
 using ProgressMeter
@@ -16,39 +17,39 @@ bcdata = bcdata[bcdata.predation .> 0, :]
 bcdata.ϕ = bcdata.latitude.*(π/180.0)
 bcdata.λ = bcdata.longitude.*(π/180.0)
 
-function haversine(p1, p2, r)
-	return acos(sin(p1[2])*sin(p2[2])+cos(p1[2])*cos(p2[2])*cos(p2[1]-p1[1]))*r
-	# ACOS(SIN(Lat1)*SIN(Lat2) +COS(Lat1)*COS(Lat2)*COS(Lon2-Lon1)) *6371
+# Get a template layer and fill it with the haversine distance
+bc = worldclim(1:19)
+zbc = [z(b) for b in bc]
+bc1 = bc[1]
+
+gdis = similar(bc1)
+bdis = similar(bc1)
+
+for i in 1:19
+   nc = Symbol("zbc"*string(i))
+   oc = Symbol("bc"*string(i))
+   bcdata[nc] = [z(v, bc[i]) for v in bcdata[oc]]
 end
 
-# Get a template layer and fill it with the haversine distance
-bc1 = worldclim(1)
-gdis = similar(bc1)
-lr = (minimum(bcdata.longitude), maximum(bcdata.longitude))
-bt = (minimum(bcdata.latitude), maximum(bcdata.latitude))
-gdis = gdis[lr, bt]
+bc_colnames = Symbol.("zbc".*string.(1:19))
+bc_webs = convert(Matrix, bcdata[:,bc_colnames])
+
+
 all_cells = [(b.λ, b.ϕ) for b in eachrow(bcdata)]
 @showprogress for lon in longitudes(gdis)
 	for lat in latitudes(gdis)
 		cell = (lon*π/180.0, lat*π/180.0)
 		if !isnan(gdis[lon,lat])
+         cell_bc_values = [zb[lon,lat] for zb in zbc]
+         cell_bc_distance = vec(sum(sqrt.((bc_webs.-cell_bc_values').^2.0); dims=2))
 			all_dist = [haversine(c, cell, 6371.0) for c in all_cells]
 			gdis[lon,lat] = mean(sort(all_dist)[1:5])
+         bdis[lon,lat] = mean(sort(cell_bc_distance)[1:5])
 		end
 	end
 end
 
-function z!(x::T) where {T <: SimpleSDMResponse}
-	μ = mean(filter(!isnan, x.grid))
-	σ = std(filter(!isnan, x.grid))
-	for i in eachindex(x.grid)
-		if !isnan(x[i])
-			x.grid[i] = (x.grid[i]-μ)/σ
-		end
-	end
-end
-
-# Add a CAP
+# Add a CAP at 4000 km
 for i in eachindex(gdis.grid)
 	if gdis.grid[i] > 4000
 		gdis.grid[i] = 4000
@@ -57,5 +58,8 @@ end
 
 heatmap(longitudes(gdis), latitudes(gdis), gdis.grid, c=:viridis)
 savefig(joinpath(@__DIR__, "..", "figures", "figure_04_a.png"))
+
+heatmap(longitudes(bdis), latitudes(bdis), log10.(bdis.grid.+1.0), c=:YlGnBu)
+savefig(joinpath(@__DIR__, "..", "figures", "figure_04_b.png"))
 
 end
