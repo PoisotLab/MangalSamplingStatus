b0853a9bc6d507827c0b7dd97b8026da42a17f33
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-11-18 11:37:41 -0500

    01 get metadata

--- scripts/01_get_metadata.jl
+++ scripts/01_get_metadata.jl
@@ -4,3 +4,34 @@ using CSV
 using GeoInterface
 
 println("01")
+
+const number_of_networks = count(MangalNetwork)
+page = 0
+all_networks = networks()
+while length(all_networks) < number_of_networks
+   global page = page + 1
+   @info "page $(page)"
+   append!(all_networks, networks("page" => page))
+end
+
+network_metadata = DataFrame()
+network_metadata.id = [n.id for n in all_networks]
+network_metadata.name = [n.name for n in all_networks]
+network_metadata.date = [n.date for n in all_networks]
+
+function get_coordinates(n::MangalNetwork; dims::Int64=1)
+   if ismissing(n.position)
+      return missing
+   end
+   if !(typeof(n.position) <: GeoInterface.Point)
+      return missing
+   end
+   return coordinates(n.position)[dims]
+end
+longitude(n::MangalNetwork) = get_coordinates(n; dims=1)
+latitude(n::MangalNetwork) = get_coordinates(n; dims=2)
+
+network_metadata.latitude = latitude.(all_networks)
+network_metadata.longitude = longitude.(all_networks)
+
+CSV.write(joinpath("data", "network_metadata.csv"), network_metadata)
