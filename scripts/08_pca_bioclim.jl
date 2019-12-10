6b3560a82d9c5780dceef4a5d9eaba1a45015144
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-12-10 08:29:53 -0500

    include xlabels

--- scripts/08_pca_bioclim.jl
+++ scripts/08_pca_bioclim.jl
@@ -25,18 +25,15 @@ para = bcdata[bcdata.parasitism.>0,:]
 mutu = bcdata[bcdata.mutualism.>0,:]
 pred = bcdata[bcdata.predation.>0,:]
 
-scatter(para.pc1, para.pc2, frame=:origin, c="#e69f00", lab="Parasitism", legend=:bottomleft, dpi=200)
+scatter(para.pc1, para.pc2, frame=:origin, c="#e69f00", lab="Parasitism", legend=:bottomleft, dpi=200, xlabel="Position on PC1", ylabel="Position on PC2")
 scatter!(mutu.pc1, mutu.pc2, c="#56b4e9", lab="Mutualism")
 scatter!(pred.pc1, pred.pc2, c="#009e73", lab="Predation")
-xaxis!("Position on PC1")
-yaxis!("Position on PC2")
 savefig(joinpath("figures", "networks_pca.png"))
 savefig(joinpath("figures", "networks_pca.pdf"))
 
-density(para.pc1, c="#e69f00", lab="Parasitism", dpi=200, frame=:box)
+density(para.pc1, c="#e69f00", lab="Parasitism", dpi=200, frame=:box, xlabel="Position on PC1")
 density!(mutu.pc1, c="#56b4e9", lab="Mutualism")
 density!(pred.pc1, c="#009e73", lab="Predation")
-xaxis!("Position on PC1")
 savefig(joinpath("figures", "position_on_pc1.png"))
 savefig(joinpath("figures", "position_on_pc1.pdf"))
 
