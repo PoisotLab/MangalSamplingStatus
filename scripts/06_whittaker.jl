abac6528a4e59cef37dd5318acabc39bdcdeb6dc
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-08-07 15:37:19 -0400

    update to the whittaker panel

--- scripts/06_whittaker.jl
+++ scripts/06_whittaker.jl
@@ -1,6 +1,8 @@
 using DataFrames
 using CSV
 using Plots
+using Plots.PlotMeasures
+using SimpleSDMLayers
 
 mangal = CSV.read(joinpath("data", "network_data.csv"))
 
@@ -15,12 +17,27 @@ n_para = size(para)[1]
 n_mutu = size(mutu)[1]
 n_pred = size(pred)[1]
 
-# And plot
-scatter(para.bc12./10.0, para.bc1, lab="Parasitism (n=$n_para)", c="#e69f00", frame=:box, dpi=200)
-scatter!(mutu.bc12./10.0, mutu.bc1, lab="Mutualism (n=$n_mutu)", c="#56b4e9")
-scatter!(pred.bc12./10.0, pred.bc1, lab="Predation (n=$n_pred)", c="#009e73")
-xaxis!("Annual precipitation", [0.0, 400.0])
-yaxis!("Average temperature", :flip, [-20.0,30.0])
+b1 = worldclim(1; bottom = -60.0)
+b12 = broadcast(n -> n/10.0, worldclim(12; bottom = -60.0))
 
+# And plot
+histogram2d(
+    b12,
+    b1,
+    frame = :box,
+    dpi = 200,
+    margin = 10mm,
+    foreground_color_legend = nothing,
+    legend = :outertopright,
+    colorbar = false,
+    lab = "",
+    c = [:lightgrey, :black],
+    bins = 50
+)
+scatter!(para.bc12 ./ 10.0, para.bc1, lab = "Parasitism", c = "#e69f00", ms = 3, msw = 0.0)
+scatter!(mutu.bc12 ./ 10.0, mutu.bc1, lab = "Mutualism", c = "#56b4e9", ms = 3, msw = 0.0)
+scatter!(pred.bc12 ./ 10.0, pred.bc1, lab = "Predation", c = "#009e73", ms = 3, msw = 0.0)
+xaxis!("Annual precipitation", (0.0, 800.0))
+yaxis!("Average temperature", :flip, [-20.0, 30.0])
 savefig(joinpath("figures", "networks_by_biomes.png"))
 # savefig(joinpath("figures", "networks_by_biomes.pdf"))
