93e0a4deafeffb97e36b24bbed68e4d83da98be7
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-01-28 12:36:56 -0500

    fix join

--- scripts/04_merge_data.jl
+++ scripts/04_merge_data.jl
@@ -5,6 +5,6 @@ network_metadata = DataFrame(CSV.File(joinpath("data", "network_metadata.csv")))
 network_interactions = DataFrame(CSV.File(joinpath("data", "network_interactions.csv")))
 network_bioclim = DataFrame(CSV.File(joinpath("data", "network_bioclim.csv")))
 
-network_data = join(join(network_metadata, network_interactions, on=:id), network_bioclim, on=:id)
+network_data = leftjoin(leftjoin(network_metadata, network_interactions, on=:id), network_bioclim, on=:id)
 
 CSV.write(joinpath("data", "network_data.csv"), network_data)
